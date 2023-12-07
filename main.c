e77dbd21d055ea4c27dfc4532162b6d8d3135547
Lee SonJae <sfg4862@gmail.com>
2023-12-07 23:03:12 +0900

    FIXXING

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <Windows.h>
 
 int main(void) {
-	int choose;
+	int choose,num1,num2;
+	char x;
 	printf("------------------------\n");
 	printf("    Basic Calculator!\n");
 	printf("------------------------\n");
 	printf("수식입력 (정수 연산자 정수) :");
+	scanf("%d %c %d", &num1, choose, &num2);
+	
+	system("cls");
+	switch (x) {
+	case '+':
+		printf("결과 :", add(num1, num2)); break;
+	case '-':
+		printf("결과 :", sub(num1, num2)); break;
+	}
 
 }
