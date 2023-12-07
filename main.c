336f83b41be16f8ce48f00a80d26f27952de0538
Lee SonJae <sfg4862@gmail.com>
2023-12-07 22:30:01 +0900

    UI UPDATE 1

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
-	
+	int choose;
+	printf("------------------------\n");
+	printf("    Basic Calculator!\n");
+	printf("------------------------\n");
+	printf("수식입력 (정수 연산자 정수) :");
+
+
 }
