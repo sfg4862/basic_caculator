e113915452b2db3d9bed96b3ce174fb9bd1aaa1d
Lee SonJae <sfg4862@gmail.com>
2023-12-07 22:41:03 +0900

    SAVE main.c 1041

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 int main(void) {
-	int choose;
+	int choose,num1,num2;
+	char x;
 	printf("------------------------\n");
 	printf("    Basic Calculator!\n");
 	printf("------------------------\n");
 	printf("수식입력 (정수 연산자 정수) :");
+	scanf("%d %c %d", &num1, choose, &num2);
 
 }
